45b44cd297bd3687fe2533626d2dda2ec32ff227
enrick-dev <enricksantos03@gmail.com>
2024-08-20 00:09:59 -0300

    created function GetBooks

--- intermal/service/book.go
+++ intermal/service/book.go
@@ -30,3 +30,24 @@ func (s *BookService) CreateBook(book *Book) error {
 	return nil
 
 }
+
+func (s *BookService) GetBooks() ([]Book, err) {
+	query := `
+		SELECT id, title, author, genre FROM books
+	`
+	rows, err := s.db.Query(query)
+	if err != nil {
+		return nil, err
+	}
+
+	var books []Book
+	for rows.Next() {
+		var book Book
+		err := rows.Scan(&book.ID, &book.Title, &book.Author, &book.Genre)
+		if err != nil {
+			return nil, err
+		}
+		books = append(books, book)
+	}
+	return books, nil
+}
