8c595fcf904bfe8409b2c183d9fc9e2b787b2a4a
enrick-dev <enricksantos03@gmail.com>
2024-08-20 00:05:22 -0300

    criado struct Book e func de Criação de Book

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/enrick-dev/gobooks.git
+
+go 1.22.3
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func sum(x, y int) (int, error) {
+	if x+y > 5 {
+		return x + y, nil
+	}
+	return 0, errors.New("x+y is less than 5")
+}
+
+func main() {
+	x, err := sum(1, 4)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(x)
+}
